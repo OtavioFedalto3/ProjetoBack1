e1b8b8ac51494a9252e1bfc16c69c39567ccd58c
otavio <otaviofedaltogame@gmail.com>
2024-03-26 14:56:50 -0300

    first commit

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,11 +1,43 @@
+import chalk from "chalk";
+
+async function checaStatus(listaURLs){
+    const arrStatus = await Promise.all(
+            listaURLs.map(async(url)=>{
+                try{
+                    const response = await fetch(url);
+                    return response.status;
+                }
+                catch(erro){
+                    return manejaErros(erro);
+                }
+        })
+    )
+    return arrStatus;
+}
 
 function extraiLinks(arrLinks){
     return arrLinks.map((objetoLink)=>Object.values(objetoLink).join());
 }
 
-
+function manejaErros(){
+    // console.log(chalk.red("algo deu errado"))
+    if(erro.cause.code==="ENOTFOUND"){
+        return "link não encontrado"
+    }else{
+        return "ocorreu um erro"
+    }
+}
 
 //criar uma função listaValidada
-export default function listaValidada(listaDeLinks){
-    return extraiLinks(listaDeLinks);
+export default async function listaValidada(listaDeLinks){
+    //return extraiLinks(listaDeLinks);
+    const links = await extraiLinks(listaDeLinks);
+    const status = await checaStatus(links);
+    //console.log(status)
+    return listaDeLinks.map((objeto,indice)=>({
+        ...objeto,
+        status:status[indice]
+    }));
 }
+
+//[gatinho salsicha](http://gatinhosalsicha.com.br/)
